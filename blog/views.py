3608b66bdc15652d0e61f6343f7961ea30c813d0
Gulufuu <Gulufuu@Gulufuus-MacBook-Pro.local>
2016-06-23 16:41:06 +0700

    create index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from blog.models import BlogPost
 
 # Create your views here.
+
+
+def homepage(request):
+    blog_list = BlogPost.objects.all()
+    data = {
+        'title': 'This is title',
+        'data1': 'Hooooorey!',
+        'blog_list': blog_list,
+    }
+    return render_to_response('index.html', data)
